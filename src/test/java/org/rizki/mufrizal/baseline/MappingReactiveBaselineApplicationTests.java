ce1c572a0db48144403397bea481ab9f58b9edc2
RizkiMufrizal <mufrizalrizki@gmail.com>
2022-12-22 22:25:35 +0700

    initiate project

---
+++ src/test/java/org/rizki/mufrizal/baseline/MappingReactiveBaselineApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rizki.mufrizal.baseline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MappingReactiveBaselineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
